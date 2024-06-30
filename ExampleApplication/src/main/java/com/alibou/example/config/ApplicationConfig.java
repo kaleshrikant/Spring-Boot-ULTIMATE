6a2ebd82f29685462e832e92f706a4a52ccca4ec
Shrikant Kale <kaleshrikant@live.com>
2024-06-30 14:40:58 +0530

    Profiles - @Bean level programmatic approach

--- ExampleApplication/src/main/java/com/alibou/example/config/ApplicationConfig.java
+++ ExampleApplication/src/main/java/com/alibou/example/config/ApplicationConfig.java
@@ -4,11 +4,13 @@ import com.alibou.example.bean.MyFirstClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean("bean1")
+    @Profile("sit")
     // @Qualifier("bean-1")
     public MyFirstClass myFirstBean() {
         return new MyFirstClass("First Bean");
