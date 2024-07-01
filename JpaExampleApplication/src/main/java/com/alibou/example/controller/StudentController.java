97503286e3d6cc289b416c3a467f0508ecaf2298
Shrikant Kale <kaleshrikant@live.com>
2024-07-01 19:35:33 +0530

    find + findAll Student(s)

--- JpaExampleApplication/src/main/java/com/alibou/example/controller/StudentController.java
+++ JpaExampleApplication/src/main/java/com/alibou/example/controller/StudentController.java
@@ -2,10 +2,10 @@ package com.alibou.example.controller;
 
 import com.alibou.example.entity.Student;
 import com.alibou.example.service.StudentService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -22,5 +22,17 @@ public class StudentController {
             @RequestBody Student student
     ) {
         return studentService.save(student);
+    }
+
+    @GetMapping("/students")
+    public List<Student> findAllStudents() {
+        return studentService.findAll();
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Optional<Student> findStudent(
+            @PathVariable("student-id") Integer studentId
+    ) {
+        return studentService.findStudent(studentId);
     }
 }
--- JpaExampleApplication/src/main/java/com/alibou/example/service/StudentService.java
+++ JpaExampleApplication/src/main/java/com/alibou/example/service/StudentService.java
@@ -5,6 +5,9 @@ import com.alibou.example.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -17,5 +20,13 @@ public class StudentService {
 
     public Student save(Student student) {
         return studentRepository.save(student);
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> findStudent(Integer studentId) {
+        return studentRepository.findById(studentId);
     }
 }
